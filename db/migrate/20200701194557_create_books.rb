fff44fabbe8023174c650fb7f7a26c5db743f373
vagrant <vagrant@localhost.localdomain>
2020-07-03 18:25:31 +0000

    making

--- db/migrate/20200701194557_create_books.rb
+++ db/migrate/20200701194557_create_books.rb
@@ -2,9 +2,7 @@ class CreateBooks < ActiveRecord::Migration[5.2]
   def change
     create_table :books do |t|
       t.string :title
-      t.string :string
       t.string :body
-      t.string :text
 
       t.timestamps
     end
